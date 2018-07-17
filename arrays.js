5f54dd6d7696e3fa9b960157869f852ecd5cb497
kevinyhong <kevinyaohuiong@gmail.com>
2018-07-17 04:41:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1)
-  return array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
